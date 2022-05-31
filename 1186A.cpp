2748cb69f93d8527d38217c7e376b8c01bd01311
praball <92200181+praball@users.noreply.github.com>
2022-05-31 11:54:27 +0530

    Add files via upload

---
+++ 1186A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(b>=a && c>=a)
+    {
+        cout<<"Yes\n";
+    }
+    else
+    {
+        cout<<"No\n";
+    }
+}
