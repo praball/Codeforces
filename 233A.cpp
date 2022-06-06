8cb0a6314112cbb39ebdfbb52844bac3359fa6f1
praball <92200181+praball@users.noreply.github.com>
2022-06-07 01:07:51 +0530

    Add files via upload

---
+++ 233A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int n;
+    cin >> n;
+    if(n%2==1)
+    {
+        cout<<-1<<endl;
+    }
+    else
+    {
+        cout<<"2 1";
+        for(int i=3;i<n;i=i+2)
+        {
+            cout<<" "<<i+1<<" "<<i<<"\n";
+        }
+    }
+    return 0;
+}
