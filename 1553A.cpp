b459744d3b4c1212bf5c2c5dfa7a4fdcbfe87d1a
praball <92200181+praball@users.noreply.github.com>
2022-06-21 15:58:31 +0530

    Add files via upload

---
+++ 1553A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int l=0;l<t;l++)
+    {
+        long long n;
+        cin>>n;
+        cout<<(n+1)/10<<"\n";
+    }
+}
