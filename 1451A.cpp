b03b34b28e848a099bb60ee970d0b55810ed1b2a
Prabal Pophaley <92200181+praball@users.noreply.github.com>
2022-06-29 17:00:06 +0530

    Add files via upload

---
+++ 1451A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for(int l=0;l<t;l++)
+    {
+        long long n;
+        cin>>n;
+        int res = sqrt(n);
+        cout<<res<<"\n";
+    }
+}
