690b15f569cb863eca938e4aa3ba9a5dfa5688c2
Prabal Pophaley <92200181+praball@users.noreply.github.com>
2022-12-14 14:05:40 +0530

    Add files via upload

--- 1619B.cpp
+++ 1619B.cpp
@@ -1,76 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/*
-bool isPerfectSquare(long long x)
-{
-    if (x >= 0) {
- 
-        long long sr = sqrt(x);
-         
-        return (sr * sr == x);
-    }
-
-    return false;
-}
-
-bool perfectCube(long long N)
-{
-    long long cube_root;
-    cube_root = round(cbrt(N));
- 
-    if (cube_root * cube_root * cube_root == N)
-	{
-        return true;
-    }
-    else
-	{
-        return false;
-    }
-}
- 
-
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int t;
-	cin>>t;
-	for(int j=0;j<t;j++)
-	{
-		long long n, count = 0;
-		cin>>n;
-		for(long long i=1;i<=n;i++)
-		{
-			if(isPerfectSquare(i)==true || perfectCube(i)==true)
-			{
-				count++;
-			}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for(int l=0;l<t;l++)
+    {
+        int n;
+        cin>>n;
+        set<int>s;
+        for(int i=1;i*i<=n;i++)
+        {
+        	s.insert(i*i);
 		}
-		cout<<count<<"\n";
-	}
-}
-*/
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int t;
-	cin>>t;
-	for(int j=0;j<t;j++)
-	{
-		int n;
-		cin >> n;
-		set<int> likes;
-		for (int i = 1; (i * i) <= n; ++i) 
+		for(int i=1;i*i*i<=n;i++)
 		{
-			likes.insert(i * i);
+			s.insert(i*i*i);
 		}
-		for (int i = 1; (i * i * i) <= n; ++i) 
-		{
-			likes.insert(i * i * i);
-		}
-		cout << likes.size()<<"\n";
-	}
+		cout<<s.size()<<"\n";
+    }
 }
